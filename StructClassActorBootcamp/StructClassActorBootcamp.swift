8a48405f8ba20fa9b4956c0611f345aa71d47e20
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-08-08 08:12:33 +0200

    first commit: Load initial files

---
+++ StructClassActorBootcamp/StructClassActorBootcamp.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StructClassActorBootcamp
+//
+//  Created by Marcin Jędrzejak on 08/08/2023.
+//
+
+import SwiftUI
+
+struct StructClassActorBootcamp: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct StructClassActorBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StructClassActorBootcamp()
+    }
+}
---
+++ StructClassActorBootcamp/StructClassActorBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructClassActorBootcampApp.swift
+//  StructClassActorBootcamp
+//
+//  Created by Marcin Jędrzejak on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct StructClassActorBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StructClassActorBootcamp()
+        }
+    }
+}
